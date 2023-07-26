98111fc36f22273e2dae58c4dbc65b0af878cd36
Brayan Durán Medina <brayan.duran@leaguesofcode.com>
2023-07-26 23:13:20 +0200

    Player added

---
+++ src/Player.ts
@@ -0,0 +1,13 @@
+class Player {
+  private id: string;
+  private x: number;
+  private y: number;
+
+  constructor(id: string) {
+    this.id = id;
+    this.x = 0;
+    this.y = 0;
+  }
+}
+
+export default Player;
