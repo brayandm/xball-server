11a1916abd8cd2ca8727303f8d3b71cdb4acba92
Brayan Durán Medina <brayan.duran@leaguesofcode.com>
2023-07-26 23:15:07 +0200

    updating player

--- src/Player.ts
+++ src/Player.ts
@@ -8,6 +8,35 @@ class Player {
     this.x = 0;
     this.y = 0;
   }
+
+  public getId() {
+    return this.id;
+  }
+
+  public getX() {
+    return this.x;
+  }
+
+  public getY() {
+    return this.y;
+  }
+
+  public getPosition() {
+    return { x: this.x, y: this.y };
+  }
+
+  public setX(x: number) {
+    this.x = x;
+  }
+
+  public setY(y: number) {
+    this.y = y;
+  }
+
+  public setPosition(x: number, y: number) {
+    this.x = x;
+    this.y = y;
+  }
 }
 
 export default Player;
