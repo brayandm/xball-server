372b152f088cb7d1a5a70ca15a941de3f8d31e1f
Brayan Durán Medina <brayan.duran@leaguesofcode.com>
2023-07-26 23:13:13 +0200

    websocket server added

---
+++ src/WebSocketServer.ts
@@ -0,0 +1,14 @@
+import * as dotenv from "dotenv";
+import WebSocketManager from "./WebSocketManager";
+import GameManager from "./GameManager";
+import EventManager from "./EventManager";
+
+dotenv.config();
+
+const webSocketManager = new WebSocketManager(
+  Number(process.env.WEBSOCKET_PORT || "3001")
+);
+
+const gameManager = new GameManager();
+
+const eventManager = new EventManager(gameManager, webSocketManager);
